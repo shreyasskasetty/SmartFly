4c5cb6b127004daac089e23a91220a913d553ac8
AshwinRaja941 <ashwinraja941@tamu.edu>
2024-01-27 23:14:59 -0600

    database integration

--- airport_time_management/misc_time/models.py
+++ airport_time_management/misc_time/models.py
@@ -1,3 +1,12 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    shop_type = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag)
--- airport_time_management/misc_time/views.py
+++ airport_time_management/misc_time/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+import random
+from datetime import datetime
+from misc_time.models import Shop
+
+def get_shop(request):
+        shops = Shop.objects.all()
+        # Serialize the data to JSON
+        shop_list = []
+        for shop in shops:
+            shop_dict = {
+                'name': shop.name,
+                'latitude': shop.latitude,
+                'longitude': shop.longitude,
+                'shop_type': shop.shop_type,
+                'tags': [tag.name for tag in shop.tags.all()],
+            }
+            shop_list.append(shop_dict)
+
+        return JsonResponse({'shops': shop_list})
